791cd83d8308d71c4b6e55876b7b9eba6f13bd49
Dogao23 <90581194+Dogao23@users.noreply.github.com>
2021-11-09 19:54:28 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:'38111',
+                nome: 'Fulano de Tal',
+                partido: 'NBA',
+                fotos: [
+                {URL:'russel-westbrook.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano de Oliveira' ,
+                partido: 'nojsjo',
+                fotos:[
+                    {URL:'prefeito-lebron.jpg', legenda: 'Verador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                   numero: '99',
+                  nome: 'Ciclano',
+                  partido: 'ABA',
+                  vice: 'Cicero',
+                  fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg',legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero:'84',
+                nome:'Josjo',
+                partido: 'jodjodjo',
+                vice:'UL',
+                fotos:[
+                {url:'84.jpg', legenda: 'Prefeito'},
+                {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
+
